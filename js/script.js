3f0924ae41b3e7febb447fa1b900cccc62354087
zahidbalooshi <zahid.balooshi@gmail.com>
2025-09-30 18:59:43 +0300

    Added Click  Event Listener and Function

--- js/script.js
+++ js/script.js
@@ -9,4 +9,15 @@ const scoreEl = document.querySelector("#score")
 
 /*Functions --------------------------------*/
 
+const handleClick = () => {
+  score = score + 10
+  scoreEl.textContent = score
+  console.log("BOOP WORKS!")
+  console.log(score)
+}
+
 /*Event Listeners -----------------------------*/
+
+boxEls.forEach((boxEl) => {
+  boxEl.addEventListener("click", handleClick)
+})
