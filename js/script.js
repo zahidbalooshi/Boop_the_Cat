28aeae21d57dc8f483445a80e6c83435baa3860e
zahidbalooshi <zahid.balooshi@gmail.com>
2025-09-30 18:57:39 +0300

    Setup Base JavaScript Elements

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*Constants --------------------------------*/
+let box
+let score = 0
+/*Variables (state) ----------------------------*/
+
+/*Cached Element References ------------------------*/
+const boxEls = document.querySelectorAll(".box")
+const scoreEl = document.querySelector("#score")
+
+/*Functions --------------------------------*/
+
+/*Event Listeners -----------------------------*/
