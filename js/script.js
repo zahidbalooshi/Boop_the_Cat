69832d841c9d74c336dd4cb2534e078cba757975
zahidbalooshi <zahid.balooshi@gmail.com>
2025-10-02 13:30:36 +0300

    Assigned specific keyboard keys to each box

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ let score = 0
 
 /*Cached Element References ------------------------*/
 const boxEls = document.querySelectorAll(".box")
+// const boxIndex = document.querySelectorAll("#id")
 const scoreEl = document.querySelector("#score")
 
 /*Functions --------------------------------*/
@@ -20,4 +21,46 @@ const handleClick = () => {
 
 boxEls.forEach((boxEl) => {
   boxEl.addEventListener("click", handleClick)
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "S" || event.key === "s") {
+    handleClick(boxEls[0])
+    console.log("S works")
+  }
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "D" || event.key === "d") {
+    handleClick(boxEls[1])
+    console.log("D works")
+  }
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "F" || event.key === "f") {
+    handleClick(boxEls[2])
+    console.log("F works")
+  }
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "J" || event.key === "j") {
+    handleClick(boxEls[3])
+    console.log("J works")
+  }
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "K" || event.key === "k") {
+    handleClick(boxEls[4])
+    console.log("K works")
+  }
+})
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "L" || event.key === "l") {
+    handleClick(boxEls[5])
+    console.log("L works")
+  }
 })
